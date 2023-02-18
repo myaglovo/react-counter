ae3fa62c0c62736ff056465e7a0a02d26234d890
Антон Кошкин <antonkoskin@MacBook-Air-Anton.local>
2023-02-18 15:45:50 +0300

    Add counter app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(1);
+  const decreaseCount = () => {
+    setCount((count) => count - 1);
+  };
+  const increaseCount = () => {
+    setCount((count) => count + 1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div>
+        <h2>Счетчик:</h2>
+        <h1>{count}</h1>
+        <button
+          disabled={count === 0 && "true"}
+          className="minus"
+          onClick={decreaseCount}
         >
-          Learn React
-        </a>
-      </header>
+          - Минус
+        </button>
+        <button
+          disabled={count >= 10 && "true"}
+          className="plus"
+          onClick={increaseCount}
+        >
+          Плюс +
+        </button>
+      </div>
     </div>
   );
 }
